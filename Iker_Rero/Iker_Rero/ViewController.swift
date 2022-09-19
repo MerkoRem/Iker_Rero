afa8141919f9b5f92d27bda43ede6b57f4539c6b
Iker Rero <ikerreromartinez@gmail.com>
2022-09-19 20:31:19 +0200

    Inicialización proyecto

---
+++ Iker_Rero/Iker_Rero/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Iker_Rero
+//
+//  Created by Alumnos on 19/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("");
+        //esto no funciona
+    }
+
+
+}
+
